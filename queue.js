ec7738f13ae4c01d2a1969a3b2d97d780c6ac808
Ben Brown <ben@ben-brown.co>
2016-11-09 22:53:00 +0000

    Create queue.js
    Basic 'Queue' implementation

---
+++ queue.js
@@ -0,0 +1,24 @@
+class Queue {
+  constructor() {
+    this.queue = [];
+  }
+  add(data) {
+    this.queue.push(data);
+  }
+  remove() {
+    if (!this.queue.length) console.error('Queue is empty!');
+    this.queue.shift();
+  }
+  access() {
+    if (!this.queue.length) return 'Queue is empty!';
+    return this.queue.shift();
+  }
+  length() {
+    return this.queue.length;
+  }
+  print() {
+    console.log(this.queue.join(' '));
+  }
+}
+
+const example = new Queue();
