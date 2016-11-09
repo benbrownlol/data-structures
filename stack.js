26baadf53643f29831d95b971ba9ac402961a58f
Ben Brown <ben@ben-brown.co>
2016-11-09 22:52:26 +0000

    Create stack.js
    Basic 'Stack' implementation

---
+++ stack.js
@@ -0,0 +1,24 @@
+class Stack {
+  constructor() {
+    this.stack = [];
+  }
+  add(data) {
+    this.stack.push(data);
+  }
+  remove() {
+    if (!this.stack.length) console.error('Stack is empty!');
+    this.stack.pop();
+  }
+  access() {
+    if (!this.stack.length) return 'Stack is empty!';
+    return this.stack.pop();
+  }
+  length() {
+    return this.stack.length;
+  }
+  print() {
+    console.log(this.stack.join(' '));
+  }
+}
+
+const example = new Stack();
